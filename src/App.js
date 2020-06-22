20e019669016c3a96e13607ba579959939136fcf
Gaurav Krishnan <gauravk2@illinois.edu>
2020-06-21 23:47:14 -0400

    Building nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar, Nav, Form, FormControl, Button, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+import "bootstrap/dist/css/bootstrap.css";
+import  './App.css'; // CSS stylesheet
+
+import Home from "./components/home";
+import Profile from "./components/profile";
+import Practice from "./components/practice";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <Navbar bg="dark" variant="dark">
+            <LinkContainer to="/">
+              <Navbar.Brand href="#home">Scriptio</Navbar.Brand>
+            </LinkContainer>
+            <Nav className="mr-auto">
+              <LinkContainer to="/">
+                <Nav.Link >Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/practice">
+                <Nav.Link>Profile</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/profile">
+                <Nav.Link >Practice</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar>
+          <Route path="/" exact component={Home} />
+          <Route path="/practice" component={Practice} />
+          <Route path="/profile" component={Profile} />
+        </div>
+  
+      </Router>
+    );
+  }
 }
 
 export default App;
+
+//shift + alt + F
+// import "bootstrap/dist/css/bootstrap.min.css";
---
+++ src/components/practice.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Practice extends Component {
+    render() {
+        return (
+            <div>
+                <p>Practice</p>
+            </div>
+        )
+    }
+}
