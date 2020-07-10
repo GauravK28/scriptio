cf9fd12b5bf454cafc01dba921f8def6acca54c4
Gaurav Krishnan <gauravk2@illinois.edu>
2020-07-10 14:31:10 -0400

    Adding client to parent repo

---
+++ client/src/view/pages/home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import { Button } from 'react-bootstrap';
+import Game from "./game/game";
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shouldLoadGame: false,
+        }
+    }
+
+    loadGame() {
+        this.setState({
+            shouldLoadGame: true,
+        })
+    }
+
+    render() {
+        if (this.state.shouldLoadGame) {
+            return (<Game />)
+        }
+        else {
+            return (
+                <div className="container">
+                    <div className="card-container">
+                        <p>Welcome to Script.io</p>
+                        <div>
+                            <Button variant="primary" onClick={() => this.loadGame()}>Start Game </Button>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+}
+
+export default Home;
