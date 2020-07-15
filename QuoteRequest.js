c19378a868440364bba050a092fb49a9ccf428bb
Gaurav Krishnan <gauravk2@illinois.edu>
2020-07-15 11:54:56 -0400

    Refactored navbar

--- QuoteRequest.js
+++ QuoteRequest.js
@@ -3,5 +3,5 @@ const axios = require('axios');
 const uri = "http://api.quotable.io/random";
 
 module.exports = getdata = () => {
-    return axios.get(uri).then(response=> response.data.content.split(" "))
+    return axios.get(uri).then(response => response.data.content.split(" "))
 }
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, } from 'react-router-dom';
-
-import { Navbar, Nav, } from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap'
 import './App.css'; // CSS stylesheet
 
@@ -10,6 +9,9 @@ import Profile from "./view/pages/profile";
 import Practice from "./view/pages/practice";
 import Settings from "./view/pages/user-settings";
 import LoginModal from "./view/pages/loginModal";
+
+import NavBar from "./view/navbar";
+import socket from './socketConfig';
 
 class App extends Component {
   constructor(props) {
@@ -29,47 +31,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <Navbar bg="light" variant="light" >
-          <div className="container">
-            <Navbar.Brand >Script.io</Navbar.Brand>
-            <Nav className="mr-auto">
-              <LinkContainer to="/">
-                <Nav.Link >Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/profile">
-                <Nav.Link>Profile</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/practice">
-                <Nav.Link >Practice</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/settings">
-                <Nav.Link >Settings</Nav.Link>
-              </LinkContainer>
-            </Nav>
-
-            <Nav className="ml-auto">
-              <Nav.Link className="float-left" onClick={this.handleModalOpen}>Login</Nav.Link>
-            </Nav>
-            <LoginModal
-              shouldModalOpen={this.state.shouldLoginModalOpen}
-            />
-          </div>
-        </Navbar>
-
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/practice">
-          <Practice />
-        </Route>
-        <Route exact path="/profile">
-          <Profile />
-        </Route>
-        <Route exact path="/settings">
-          <Settings />
-        </Route>
-      </Router>
+      <NavBar />
     );
   }
 }
