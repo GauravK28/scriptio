56d3cd50e3178a85ba3e3bc4cba806bb41d67c3e
Gaurav Krishnan <gauravk2@illinois.edu>
2020-07-17 15:45:49 -0400

    Refactored DB and added join game socket

--- server.js
+++ server.js
@@ -1,28 +1,78 @@
+// npm run dev
 const express = require('express');
+const dotenv = require('dotenv');
+// Load config
+dotenv.config({path : './config/config.env'})
+
 const app = express();
 
 const socketio = require('socket.io');
 const mongoose = require('mongoose');
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
-    console.log("server is running on port", server.address().port);
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 const io = socketio(server);
+
+const connectDB = require('./config/db');
+connectDB();
 
 
 const Game = require('./Models/Game');
 const QuoteRequest = require('./QuotableAPI');
 
-mongoose.connect('mongodb://localhost:27017/scriptio',
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => { console.log('successfully connected to database') });
 
 io.on('connect', (socket) => {
     console.log("CONNECTED");
 
+    socket.on('join-game', async ({ gameID: _id, nickName }) => {
+        try {
+            console.log("LOOK");
+            Game.find((err, game) => {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log(game);
+            });
+
+            console.log("going to join");
+            console.log(_id);
+            let g = await Game.findById(_id).exec();
+            console.log(g);
+
+            if (g.isOpen) {
+                const gameID = g._id.toString();
+                socket.join(gameID);
+                let player = {
+                    socketID: socket.id,
+                    isPartyLeader: false,
+                    nickName
+                }
+                g.players.push(player);
+                g.save(function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log('Success!');
+                });
+                io.to(gameID).emit('update-game', g);
+                console.log("PLAYER JOINED");
+            }
+        } catch (error) {
+            console.log("here?");
+            console.log(error);
+
+        }
+    });
+
     socket.on('create-game', async (nickName) => {
         try {
+            console.log("Testing to make sure it is connected");
+            mongoose.connection.on('error', function (err) {
+                console.error('MongoDB error: %s', err);
+            });
+
             const quotableData = await QuoteRequest();
 
             let game = new Game();
@@ -34,14 +84,19 @@ io.on('connect', (socket) => {
             }
             game.players.push(player);
             //game = await game.save();
-            game.save(await function (err) {
-                if (!err) console.log('Success!');
+            await game.save((err) => {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log('Success!');
             });
 
             const gameID = game._id.toString();
+            console.log(game._id);
             socket.join(gameID);
             io.to(gameID).emit('update-game', game);
             console.log("GAME WAS CREATED");
+            console.log(game);
         } catch (error) {
             console.log(error);
         }
